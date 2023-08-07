78fdc1f559b8adce95caeda1068385c0d8f44dbf
kibria <shan2neo@gmail.com>
2023-08-07 20:06:33 +0600

    modal setup

---
+++ src/Components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,40 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+
+type AuthModalProps = {};
+const { isOpen, onOpen, onClose } = useDisclosure();
+
+const AuthModal: React.FC<AuthModalProps> = () => {
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody></ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
--- src/Components/navbar/RightContent/RightContent.tsx
+++ src/Components/navbar/RightContent/RightContent.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import AuthModal from "@/Components/Modal/Auth/AuthModal";
 
 type RightContentProps = {};
 
 const RightContent: React.FC<RightContentProps> = () => {
   return (
     <>
+      <AuthModal />
       <AuthButtons />
     </>
   );

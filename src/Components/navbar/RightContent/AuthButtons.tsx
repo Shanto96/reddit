7a7a172f0fc2e000881c10acdc6a3d2b28e6bdc5
kibria <shan2neo@gmail.com>
2023-08-07 10:55:56 +0600

    --done auth button design

--- src/Components/navbar/RightContent/AuthButtons.tsx
+++ src/Components/navbar/RightContent/AuthButtons.tsx
@@ -6,8 +6,22 @@ type AuthButtonsProps = {};
 const AuthButtons: React.FC<AuthButtonsProps> = () => {
   return (
     <Flex align="center" justify="center">
-      <Button colorScheme="blue">Log In</Button>
-      <Button colorScheme="blue">Sign Up</Button>
+      <Button
+        variant="outline"
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+      >
+        Log In
+      </Button>
+      <Button
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+      >
+        Sign Up
+      </Button>
     </Flex>
   );
 };
--- src/chakara/Theme.tsx
+++ src/chakara/Theme.tsx
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
-// import { Button } from "./button";
+import { Button } from "./Button";
 // import { Input } from "./input";
 
 export const theme = extendTheme({
@@ -22,7 +22,7 @@ export const theme = extendTheme({
     }),
   },
   components: {
-    // Button,
+    Button,
     // Input, // not working for some reason - come back to this
   },
 });

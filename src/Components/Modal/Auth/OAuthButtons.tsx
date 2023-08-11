1b12024f2bf58728ff556abd45843970183426f7
kibria <shan2neo@gmail.com>
2023-08-11 20:45:42 +0600

    --done oauth setup

--- src/Components/Modal/Auth/OAuthButtons.tsx
+++ src/Components/Modal/Auth/OAuthButtons.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Flex, Button, Image } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 type OAuthButtonsProps = {};
 
 const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <Flex direction="column" pb={4} gap={2} width="100%">
-      <Button variant="oauth">
+      <Button
+        variant="oauth"
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
         {" "}
         <Image src="./images/googlelogo.png" height="26px" mr={4} /> Continue
         with google{" "}

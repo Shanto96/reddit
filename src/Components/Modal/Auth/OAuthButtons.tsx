0e69fae7e4918177f8be91f693042eda2d590bd4
kibria <shan2neo@gmail.com>
2023-08-17 20:53:07 +0600

    --done oAuth user save to db

--- src/Components/Modal/Auth/OAuthButtons.tsx
+++ src/Components/Modal/Auth/OAuthButtons.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Flex, Button, Image, Text } from "@chakra-ui/react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/clientApp";
+import { auth, firestore } from "@/firebase/clientApp";
 import { FIREBASE_ERRORS } from "@/firebase/errors";
+import { addDoc, collection } from "firebase/firestore";
+import { User } from "firebase/auth";
 
 type OAuthButtonsProps = {};
 
 const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, userCred, loading, error] =
+    useSignInWithGoogle(auth);
+  const createUserDocument = async (user: User) => {
+    await addDoc(
+      collection(firestore, "users"),
+      JSON.parse(JSON.stringify(user))
+    );
+  };
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
 
   return (
     <Flex direction="column" pb={4} gap={2} width="100%">
